618f6910d8f6e4313a0e7b20f4c9ff72bab00fd2
BarnabuTech <barnabutech@gmail.com>
2024-09-12 14:31:45 +0300

    Initial Commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {
-    experimental: {
-        ppr: 'incremental',
-      },
-};
+const nextConfig = {};
+//     experimental: {
+//         ppr: 'incremental',
+//       },
+// };
 
 export default nextConfig;
